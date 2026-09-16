6f8d03f07b2e011bd9c749649f13fb8f3746677e
Ema Mendoza <emamendoza.dev@gmail.com>
2026-09-16 16:57:20 -0600

    chore: bootstrap astro project with tailwind and i18n

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://emamendoza-dev.github.io',
+  base: '/emamendoza-dev-page',
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
